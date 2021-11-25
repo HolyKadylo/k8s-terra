27a0e279c99607fce82eab67633013c782ff725e
illya <pivas47@gmail.com>
2021-11-25 18:45:28 +0200

    markdown for todo

--- install.sh
+++ install.sh
@@ -19,15 +19,14 @@ echo \
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
-newgrp docker
+newgrp docker # interrupts somewhere starting from here
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
+sudo apt-get update # interrupts somewhere ending in here
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
 sudo mv daemon.json /etc/docker/
 sudo systemctl restart docker
 sudo systemctl restart kubelet
