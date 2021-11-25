236fb250564cc77cb2d382b3fbb211a231fe5afd
illya <pivas47@gmail.com>
2021-11-25 17:50:27 +0200

    work in progress

--- install.sh
+++ install.sh
@@ -28,5 +28,10 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+sudo swapoff -a
+
+mkdir -p $HOME/.kube
+sudo cp -y -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 sudo kubeadm init --apiserver-advertise-address=10.20.0.200 ### TODO variable
